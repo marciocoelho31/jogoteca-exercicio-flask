054ed59c29e46d1b88b5114c7e45e4bec2e419f7
Marcio Coelho Socorro <m.coelho.socorro@avanade.com>
2024-01-13 00:04:28 -0300

    Implementandoflask-wtf e criptografia com Bcrypt

--- helpers.py
+++ helpers.py
@@ -1,5 +1,18 @@
 from jogoteca import app
 import os
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, validators
+
+class FormularioJogo(FlaskForm):
+    nome = StringField('Nome do Jogo', [validators.DataRequired(), validators.Length(min=1, max=50)])
+    categoria = StringField('Categoria', [validators.DataRequired(), validators.Length(min=1, max=40)])
+    console = StringField('Console', [validators.DataRequired(), validators.Length(min=1, max=20)])
+    salvar = SubmitField('Salvar')
+
+class FormularioUsuario(FlaskForm):
+    nickname = StringField('Nome do usuário', [validators.DataRequired(), validators.Length(min=1, max=8)])
+    senha = PasswordField('Senha', [validators.DataRequired(), validators.Length(min=1, max=100)])
+    submit = SubmitField('Login')
 
 def recupera_imagem(id):
     for nome_arquivo in os.listdir(app.config['UPLOAD_PATH']):
@@ -12,4 +25,4 @@ def deleta_arquivo(id):
     arquivo = recupera_imagem(id)
     if arquivo != 'capa_padrao.jpg':
         os.remove(os.path.join(app.config['UPLOAD_PATH'], arquivo))
-        
+
